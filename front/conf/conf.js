63f2a61fa7a590f843bd0355f58cbd546c069bb8
matromen <matromen@naver.com>
2020-07-02 17:48:09 +0900

    frontUrl add

---
+++ front/conf/conf.js
@@ -0,0 +1,4 @@
+const frontUrl = process.env.NODE_ENV === 'product' ? 'http://api.matromen.kr' : 'http://localhost:3100/api';
+
+
+export {frontUrl};
--- front/sagas/index.js
+++ front/sagas/index.js
@@ -2,8 +2,9 @@ import {all, call} from 'redux-saga/effects';
 import user from './user';
 import post from './post';
 import axios from 'axios';
+import {frontUrl} from '../conf/conf'
 
-axios.defaults.baseURL = 'http://localhost:3100/api';
+axios.defaults.baseURL = frontUrl;
 
 export default function* rootSaga(){
     yield all([
